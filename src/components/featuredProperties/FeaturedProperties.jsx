27fe0522791e44fabd46ae80d3b68dccbc8a38d0
mixishop-ui <nguyenhoi13041999@gmail.com>
2023-12-13 00:36:51 +0700

    fix url link img

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -14,9 +14,7 @@ const FeaturedProperties = () => {
           {data.map((item) => (
             <div className="fpItem" key={item._id}>
               <img src={item?.photos[0]} alt="" className="fpImg" />
-              <span className="fpName">
-                <a href={`./hotels/${item?._id}`}>{item?.name}</a>
-              </span>
+              <span className="fpName">{item?.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">
                 Starting from ${item?.cheapestPrice}
